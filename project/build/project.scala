89ff6f4671b47f56712e8daa37cddd8f9a7a0358
Heiko Seeberger <heiko.seeberger@googlemail.com>
2010-09-21 09:32:20 +0200

    Created project with SBT 0.7.4, Scala 2.8.0 and deps on ScalaQuery 0.9.0 and SLF4S 1.0.1.

---
+++ project/build/project.scala
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2009-2010 WeigleWilczek and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+import sbt._
+
+class DemoScalaQueryProject(info: ProjectInfo) extends DefaultProject(info) {
+
+  // ===================================================================================================================
+  // Dependencies
+  // ===================================================================================================================
+
+  // Module configurations
+
+  // Compile
+  val scalaQuery = "org.scalaquery" %% "scalaquery" % "0.9.0" withSources
+  val slf4s = "com.weiglewilczek.slf4s" %% "slf4s" % "1.0.1" withSources
+
+  // Provided
+  val slf4jJdk14 = "org.slf4j" % "slf4j-jdk14" % "1.6.1" % "provided" intransitive
+
+  // Test
+  val mockito = "org.mockito" % "mockito-all" % "1.8.4" % "test" withSources
+  val specs = "org.scala-tools.testing" %% "specs" % "1.6.5" % "test" withSources
+}
